80a54f56ee8d7ca5aef6a31f351479a865237ec2
jacquesjd <jacques.dancause@gmail.com>
2022-01-20 10:22:14 -0400

    Create interface

---
+++ cropper.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+
+let btnCrop, image, pieces;
+btnCrop = document.querySelector("#btnCrop");
+image = document.querySelector("#source");
+pieces = document.querySelector("#pieces");
+
+btnCrop.onclick = function(){
+
+    
+
+};
+
+};
